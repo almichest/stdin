49e7833bfbad1e6e8ad30cf72513febb435d322b
Hiraku Ohno <hiraku.ohno@glucose.jp>
2016-12-04 20:44:11 +0900

    add stdin

--- stdin/main.swift
+++ stdin/main.swift
@@ -1,12 +1,6 @@
-//
-//  main.swift
-//  stdin
-//
-//  Created by Hiraku Ohno on 2016/12/04.
-//
-//
 
-import Foundation
-
-print("Hello, World!")
-
+extension String: CollectionType {
+    static func getValues() -> [String] {
+        return readLine()!.split(" ")
+    }
+}
