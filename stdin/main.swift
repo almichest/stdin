0ddec8d323597e851d41d6a0e2815bb839b0900d
Hiraku Ohno <hiraku.ohno@glucose.jp>
2017-03-08 23:14:55 +0900

    Add subscript

--- stdin/main.swift
+++ stdin/main.swift
@@ -3,6 +3,25 @@ import Foundation
 extension String: Collection { }
 typealias I = Int64
 
+extension Array {
+    subscript(index: I) -> Element {
+        get {
+            return self[Int(index)]
+        }
+        set {
+            self[Int(index)] = newValue
+        }
+    }
+    subscript(index: UInt) -> Element {
+        get {
+            return self[Int(index)]
+        }
+        set {
+            self[Int(index)] = newValue
+        }
+    }
+}
+
 func getValues(_ separator: Character = " ") -> [String] {
     return readLine()!.split(separator: separator)
 }
